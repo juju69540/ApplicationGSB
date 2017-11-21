7dda291b7d9d98665c941d2d3a6ded3b6269a69e
juju69540 <juju692010@live.fr>
2017-11-21 17:34:14 +0100

    Add project init
    Ajout de tous les fichiers dans le dépôt gitHub

---
+++ GSBC#/Authentification/Authentification/GSB - connecté.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Authentification
+{
+    partial class GSB___connecté
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(116, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connexion réussie";
+            // 
+            // GSB___connecté
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 315);
+            this.Controls.Add(this.label1);
+            this.Name = "GSB___connecté";
+            this.Text = "GSB___connecté";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ GSBC#/Authentification/Authentification/GSB - connecté.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authentification
+{
+    public partial class GSB___connecté : Form
+    {
+        public GSB___connecté()
+        {
+            InitializeComponent();
+        }
+    }
+}
