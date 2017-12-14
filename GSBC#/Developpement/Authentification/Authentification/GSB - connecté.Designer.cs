149b806d1ec4b7331b3e2ebf1afe5d06b74d8897
Unknown <Julian.ZERBIB@icof.fr>
2017-12-14 15:02:24 +0100

    correct

--- GSBC#/Developpement/Authentification/Authentification/GSB - connecté.Designer.cs
+++ GSBC#/Developpement/Authentification/Authentification/GSB - connecté.Designer.cs
@@ -907,21 +907,4 @@ namespace Authentification
         private System.Windows.Forms.Label lblPracRechercher;
         private System.Windows.Forms.TextBox txbPracRechercherPracticien;
     }
-    #region Windows Form Designer generated code
-        #endregion
-    /*
-        private System.Windows.Forms.TextBox txbPracContact;
-        private System.Windows.Forms.TextBox txbPracTel;
-        private System.Windows.Forms.TextBox txbPracAdresse;
-        private System.Windows.Forms.TextBox txbPracSocial;
-        private System.Windows.Forms.TextBox txbPracNom;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label lblPracSocial;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Button btnPracAnnuler; */
 }
